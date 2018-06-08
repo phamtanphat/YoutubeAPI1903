0b65d15d8397209136683c03112e945ec2cac23f
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-08 20:25:24 +0700

    "Rut gon code"

--- app/src/main/java/com/ptp/phamtanphat/youtubeapi1903/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/youtubeapi1903/MainActivity.java
@@ -9,7 +9,7 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class MainActivity extends YouTubeBaseActivity {
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     String API_KEY = "AIzaSyDRnAsb1dRo960Us8AzLkKG-g14XRYbeHY";
     YouTubePlayerView youTubePlayerView;
@@ -21,21 +21,23 @@ public class MainActivity extends YouTubeBaseActivity {
         youTubePlayerView = findViewById(R.id.youtubeplayerview);
 
         //String s =  API_KEY
-        youTubePlayerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
-            @Override
-            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-//                youTubePlayer.cueVideo();
-                youTubePlayer.loadVideo("4KJl71NE0zI");
-            }
-
-            @Override
-            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                if(youTubeInitializationResult.isUserRecoverableError()){
-                    youTubeInitializationResult.getErrorDialog(MainActivity.this,Request_Code_Youtube);
-                }
-            }
-        });
+        youTubePlayerView.initialize(API_KEY,this );
     }
+    //cach 1
+//    public YouTubePlayer.OnInitializedListener onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+//        @Override
+//        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+////                youTubePlayer.cueVideo();
+//            youTubePlayer.loadVideo("4KJl71NE0zI");
+//        }
+//
+//        @Override
+//        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+//            if (youTubeInitializationResult.isUserRecoverableError()) {
+//                youTubeInitializationResult.getErrorDialog(MainActivity.this, Request_Code_Youtube);
+//            }
+//        }
+//    };
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -55,5 +57,17 @@ public class MainActivity extends YouTubeBaseActivity {
             });
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        youTubePlayer.loadVideo("4KJl71NE0zI");
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(MainActivity.this, Request_Code_Youtube);
+        }
     }
 }
